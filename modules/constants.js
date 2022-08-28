a3d311de845661ca59e1bc3ff427be0e65b16a7b
Krituraj Anand <jaiswalkrituraj20@gmail.com>
2022-08-28 12:01:30 +0530

    refactor: extract constants into separate file

---
+++ modules/constants.js
@@ -0,0 +1,57 @@
+const WEEK_DAYS = [
+  {
+    short: "Sun",
+    long: "Sunday",
+  },
+  {
+    short: "Mon",
+    long: "Monday",
+  },
+  {
+    short: "Tue",
+    long: "Tuesday",
+  },
+  {
+    short: "Wed",
+    long: "Wednesday",
+  },
+  {
+    short: "Thu",
+    long: "Thursrday",
+  },
+  {
+    short: "Fri",
+    long: "Friday",
+  },
+  {
+    short: "Sat",
+    long: "Saturday",
+  },
+];
+const HOURS_IN_A_DAY = [
+  "All Day",
+  "12AM",
+  "1AM",
+  "2AM",
+  "3AM",
+  "4AM",
+  "5AM",
+  "6AM",
+  "7AM",
+  "8AM",
+  "9AM",
+  "10AM",
+  "11AM",
+  "12PM",
+  "1PM",
+  "2PM",
+  "3PM",
+  "4PM",
+  "5PM",
+  "6PM",
+  "7PM",
+  "8PM",
+  "9PM",
+  "10PM",
+  "11PM",
+];
