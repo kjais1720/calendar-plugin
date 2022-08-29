c083126c0c8f51042c0f589bfb210e304d442928
Krituraj Anand <jaiswalkrituraj20@gmail.com>
2022-08-29 18:24:23 +0530

    feat: create function to render day view

--- modules/day.js
+++ modules/day.js
@@ -13,3 +13,41 @@ function renderHourRowsOfADay(dateToDisplay) {
   });
   dayTableBody.innerHTML = allRows;
 }
+
+function initDayNavButtons() {
+  document
+    .getElementById(`nextDay_${this.uniqueCalendarId}`)
+    .addEventListener("click", () => {
+      this.nthDateFromCurrentDate++;
+      this.renderDayView();
+    });
+
+  document
+    .getElementById(`previousDay_${this.uniqueCalendarId}`)
+    .addEventListener("click", () => {
+      this.nthDateFromCurrentDate--;
+      this.renderDayView();
+    });
+}
+
+function renderDayView() {
+  const dayDisplay = document.getElementById(
+    `dayDisplay_${this.uniqueCalendarId}`
+  );
+  const dayHeading = document.getElementById(`dayHeading_${this.uniqueCalendarId}`);
+  const dateToDisplay = new Date();
+
+  if (this.nthDayFromCurrentDay !== 0) {
+    dateToDisplay.setDate(dateToDisplay.getDate() + this.nthDateFromCurrentDate);
+  }
+  const dateComponents = dateToDisplay.toString().split(" ");
+  const yearToDisplay = dateToDisplay.getFullYear();
+  const monthStringToDisplay = dateComponents[1];
+  let weekDayOfDateToDisplay = dateComponents[0];
+  weekDayOfDateToDisplay = WEEK_DAYS.find((day)=>day.short === weekDayOfDateToDisplay)
+
+  dayDisplay.innerText = `${monthStringToDisplay} ${dateToDisplay.getDate()}, ${yearToDisplay}`;
+  dayHeading.innerHTML = `<th></th><th>${weekDayOfDateToDisplay.long}</th>`; //Empty cell before the Week dates
+
+  renderHourRowsOfADay.call(this, dateToDisplay);
+}
