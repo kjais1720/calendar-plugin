2dec92e6349a21f8ae9fc7617ed4b3a57bac191c
Krituraj Anand <jaiswalkrituraj20@gmail.com>
2022-09-01 09:08:45 +0530

    feat(demo): initialize variables & constants

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+let renderEvents;
+let selectedEventId;
+let selectedDate;
+let selectedTime;
+const newEventModal = document.getElementById("newEventModal");
+const eventDetailsModal = document.getElementById("eventDetailsModal");
+const backDrop = document.getElementById("modalBackDrop");
+const eventTitleInput = document.getElementById("eventTitleInput");
+let eventsList = localStorage.getItem("eventsList")
+  ? JSON.parse(localStorage.getItem("eventsList"))
+  : [];
+
 function init() {
   const newCalendar = new Calendar("calendar");
   newCalendar.render();
-  addEventsPluginToCalendar(newCalendar)
+  renderEvents = addEventsPluginToCalendar(newCalendar)
 }
 
 init();
